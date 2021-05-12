851c88c1dc860f1709407d48afa69af14d5057d9
Anne-Marie Nault <anne.ma@gmail.com>
2021-05-12 13:50:05 -0400

    remove review#new route and action and embedded it on the show view for restaurants. added form to add new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,11 @@
 class ReviewsController < ApplicationController
-  def new
-    @review = Review.new
-  end
+  # def new
+  #   @review = Review.new
+  # end
 
   def create
     @review = Review.new(review_params)
-    @restaurant = Restaurant.find(params[:id])
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review.restaurant = @restaurant
     if @review.save
       redirect_to restaurant_path(@restaurant)
